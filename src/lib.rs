42bb5f2e1c93cdc0584a9e834c77314044b99fc8
tachikoma <fuchishell@gmail.com>
2026-08-24 01:20:01 +0530

    t_random_port_bind

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, get};
 
 #[get("/health_check")]
@@ -5,9 +7,9 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().service(health_check))
-        .bind("127.0.0.1:8000")?
+        .listen(listener)?
         .run();
     Ok(server)
 }
