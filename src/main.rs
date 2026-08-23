fa6c932a2cb55ca50ab3a6b8a550d096dbf31921
tachikoma <fuchishell@gmail.com>
2026-08-24 00:36:43 +0530

    t_health_check

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, get};
-
-#[get("/")]
-async fn greet() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!\n")
-}
+use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(greet))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run()?.await
 }
