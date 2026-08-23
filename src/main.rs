5030edf197e3bb27bb32b1e7d708a4d02aa2be12
tachikoma <fuchishell@gmail.com>
2026-08-23 22:52:49 +0530

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn greet() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!\n")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(greet))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
